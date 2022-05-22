31840184d720cf4c5bc28231ced90acfd18d69cc
Paula Gal <47817220+Paula-Gal@users.noreply.github.com>
2022-05-22 16:21:51 +0300

    Update main.py

--- lab6/main.py
+++ lab6/main.py
@@ -1,1 +1,13 @@
 
+
+import matplotlib.pyplot as plt
+
+# get data from .tsp file
+from utils.library import *
+
+# citire
+file = 'data/kroA100.tsp'
+tsp_data = readData(file)
+
+# afisare header fisier tsp
+displayData(tsp_data)
